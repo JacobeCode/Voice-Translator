4591636ca8f2dddde0b034d7a4e6e679ec486e7a
JacobeCode <kuba.g.code.4@gmail.com>
2022-12-29 14:51:50 +0100

    Added settings class to synthesize function
    Added settings class which contains modifiable options for synthesis operation. Added PyQt5 in VT_env for further GUI implementation.

--- Text_to_speech/settings.py
+++ Text_to_speech/settings.py
@@ -1,22 +1,23 @@
 class setup:
-    # Service setting in synthesize, because of ap
-    service=""
-    session_id=""
-    grpc_timeout=0
-    list_voices=False
-    response="streaming"
-    text=""
-    input_file=""
-    out_path="class_test_object.wav"
-    sample_rate=0
-    audio_encoding="pcm16"
-    speech_pitch=1.0
-    speech_range=1.0
-    speech_rate=1.0
-    speech_volume=1.0
-    voice_name=""
-    voice_gender="male"
-    voice_age=""
-    language=""
-    play=False
-    tls_directory=""
+    def __init__(self):
+        # Service setting in synthesize, because of ap
+        self.service=""
+        self.session_id=""
+        self.grpc_timeout=0
+        self.list_voices=False
+        self.response="streaming"
+        self.text=""
+        self.input_file=""
+        self.out_path="class_test_object_02.wav"
+        self.sample_rate=44100
+        self.audio_encoding="pcm16"
+        self.speech_pitch=1.0
+        self.speech_range=1.0
+        self.speech_rate=1.0
+        self.speech_volume=1.0
+        self.voice_name=""
+        self.voice_gender="male"
+        self.voice_age='child'
+        self.language="pl"
+        self.play=False
+        self.tls_directory=""
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Main file of project
 from Text_to_speech.synthesize import synthesize
+from Text_to_speech.settings import setup
 # Here you can insert the phrase you want to synthesize
 settings = setup()
-synthesize("Test", settings)
+synthesize("Testowanie innych ustawień naszego programu", settings)
