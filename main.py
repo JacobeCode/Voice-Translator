218ab9154674776435c15f7ac16333d5f64627ae
JacobeCode <kuba.g.code.4@gmail.com>
2023-01-03 13:05:38 +0100

    Working UI new load.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 from Text_to_speech.synthesize import synthesize
 from Text_to_speech.settings import setup
 import sys
-from UI import Ui_MainWindow
 from PyQt5 import QtWidgets, uic
 
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow.Ui_Voice_Translator):
+from UI import UIPy_MainWindow
+
+class MainWindow(QtWidgets.QMainWindow, UIPy_MainWindow.Ui_Voice_Translator):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
