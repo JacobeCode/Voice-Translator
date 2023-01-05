bebf26819ce40b325d3f2d1a1ab1a6607e59ff40
JacobeCode <kuba.g.code.4@gmail.com>
2023-01-05 22:35:17 +0100

    UI folders change

--- UI/main_window_class.py
+++ UI/main_window_class.py
@@ -1,8 +1,8 @@
-from UI import settings_window
+from UI import main_window
 
 from PyQt5 import QtWidgets
 
-class MainWindow(QtWidgets.QMainWindow, settings_window.Ui_Voice_Translator):
+class MainWindow(QtWidgets.QMainWindow, main_window.Ui_Voice_Translator):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
