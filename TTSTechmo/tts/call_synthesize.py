cfe19cbba5cc0921667ae3f37b979f61cb3a7980
JacobeCode <kuba.g.code.4@gmail.com>
2023-01-05 22:50:35 +0100

    Update call_synthesize.py

--- TTSTechmo/tts/call_synthesize.py
+++ TTSTechmo/tts/call_synthesize.py
@@ -1,10 +1,10 @@
-import Text_to_speech.techmo_tts_pb2 as techmo_tts_pb2
-import Text_to_speech.techmo_tts_pb2_grpc as techmo_tts_pb2_grpc
+import TTSTechmo.techmo_tts_pb2 as techmo_tts_pb2
+import TTSTechmo.techmo_tts_pb2_grpc as techmo_tts_pb2_grpc
 import grpc
 import os
-from Text_to_speech.audio_player import AudioPlayer
-from Text_to_speech.audio_saver import AudioSaver
-from Text_to_speech.create_channel import create_channel
+from TTSTechmo.audio_player import AudioPlayer
+from TTSTechmo.audio_saver import AudioSaver
+from TTSTechmo.create_channel import create_channel
 
 
 def call_synthesize(args, text):
