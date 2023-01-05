7a2fb7309dc2e7b5b2c13a6a73530836eaade28c
JacobeCode <kuba.g.code.4@gmail.com>
2023-01-05 20:55:00 +0100

    Changed UI main to UI settings

--- UI/main_window_class.py
+++ UI/main_window_class.py
@@ -1,8 +1,8 @@
-from UI import UIPy_MainWindow
+from UI import settings_window
 
 from PyQt5 import QtWidgets
 
-class MainWindow(QtWidgets.QMainWindow, UIPy_MainWindow.Ui_Voice_Translator):
+class MainWindow(QtWidgets.QMainWindow, settings_window.Ui_Voice_Translator):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
